78effcfdb566e0f86c160f8f1edbaf7b40386de6
niculas1989 <nicolas.maranzano@libero.it>
2022-03-01 13:42:29 +0100

    first commit

---
+++ PHP/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//| qui scriveremo del codice
+
+//! La traccia per la parte PHP:
+
+/*
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
